3a285b025a8fb45867e9055d2e47d576280747a5
P.Straub <pascal@straub.engineer>
2023-01-21 18:07:55 +0100

    update in code and tryed to create jar file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # VolumetricLawnGenerator
+
+### JAR and EXE compiling
+https://www.youtube.com/watch?v=IoPXzopsmpE
+The end is important here!!!
+https://www.youtube.com/watch?v=51iMSVUOQNM
--- src/main/java/engineer/straub/launcher/PrimaryController.java
+++ src/main/java/engineer/straub/launcher/PrimaryController.java
@@ -3,7 +3,6 @@ package engineer.straub.launcher;
 import engineer.straub.generator.ImageDraw;
 import engineer.straub.model.ImageDrawArgument;
 import engineer.straub.util.GeneratorService;
-import javafx.concurrent.Service;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.CheckBox;
@@ -11,8 +10,6 @@ import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-
-import java.awt.font.ImageGraphicAttribute;
 
 public class PrimaryController {
 
@@ -45,8 +42,6 @@ public class PrimaryController {
 
     @FXML
     private void onGenerate() {
-        System.out.println(ImageDraw.formatInt(49, 5));
-        System.out.println(ImageDraw.digitCount(1000));
         error.setText("");
         info.setText("Generating...");
         btnGenerate.setDisable(true);
@@ -87,7 +82,6 @@ public class PrimaryController {
         ImageDraw.interrupt();
     }
 
-    @FXML
     public void onGenerationFinished(String message) {
         btnGenerate.setDisable(false);
         btnCancel.setDisable(true);
@@ -97,7 +91,6 @@ public class PrimaryController {
         info.setText(message);
     }
 
-    @FXML
     public void onGenerationFailed(String errorMessage) {
         btnGenerate.setDisable(false);
         btnCancel.setDisable(true);
@@ -107,14 +100,12 @@ public class PrimaryController {
         error.setText(errorMessage);
     }
 
-    @FXML
     public void setProgressBarProgress(int width) {
         if (width < 0) width = 0;
         if (width > MAX_WINDOW_WIDTH) width = MAX_WINDOW_WIDTH;
         progressBar.setPrefWidth(width);
     }
 
-    @FXML
     public void setProgressBarColor(String color) {
         progressBar.setStyle("-fx-background-color: " + color);
     }
